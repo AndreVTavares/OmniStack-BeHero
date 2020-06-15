8435bc090719f68f68d4f4cff6022ad41a4139a3
AndreVTavares <andretavares3@gmail.com>
2020-06-15 10:17:05 -0300

    OngController and adding data on database

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 
+const OngController = require('./controllers/OngController')
+
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-    return response.json({
-        evento: 'Semana Omnistack 11',
-        aluno: 'André Tavares'
-    });
-});
+
+routes.get('/ongs', OngController.index)
+
+routes.post('/ongs', OngController.create);
 
 module.exports = routes;
