fc9ecd211663b7484c0cd9d01828c9edfd57dc34
AndreVTavares <andretavares3@gmail.com>
2020-06-23 12:15:15 -0300

    Adding valitation on backend

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -5,24 +5,35 @@ module.exports = {
     async index(request, response) {
         const { page = 1 } = request.query;
 
-        const [count] = connection('incidents').count()
+        const [count] = await connection('incidents').count()
         
-        const incidents = await connection('incidents')
-            .join('ongs', 'ong_id', '=', 'incidents.ong_id')
-            .limit(5)
-            .offset((page - 1) * 5)
-            .select([
-                'incidents.*', 
-                'ongs.name', 
-                'ongs.email', 
-                'ongs.whatsapp', 
-                'ongs.city', 
-                'ongs.uf'
-            ])
+        try {
+            
+            const incidents = await connection('incidents')
+                .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
+                .limit(5)
+                .offset((page - 1) * 5)
+                .select([
+                    'incidents.*', 
+                    'ongs.name', 
+                    'ongs.email', 
+                    'ongs.whatsapp', 
+                    'ongs.city', 
+                    'ongs.uf'
+                ])
 
-        response.header('X-Total-Count', count['count(*)'])
+            response.header('X-Total-Count', count['count(*)'])
+
+            return response.json(incidents);
+
+        } catch (error) {
+
+            console.log('Não deu certo', error);
+            
+        }
         
-        return response.json(incidents);
+
+        
     },
 
     async create(request, response) {
